b9b098b59d59f7ed5e732ce69df3d39faef8155f
Joe Charnock <62345720+JCCharnock@users.noreply.github.com>
2020-05-05 16:08:24 -0400

    Updates

---
+++ precache-manifest.06f7b42c3bcb64f9be34c641a5568acd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e395958bb76b26195e8aa97e7298ed2a",
+    "url": "/JCCharnock/React-To-Do-List/index.html"
+  },
+  {
+    "revision": "424725a07f1fe9d7d520",
+    "url": "/JCCharnock/React-To-Do-List/static/css/main.9ceb0942.chunk.css"
+  },
+  {
+    "revision": "b030a2a92880466947ee",
+    "url": "/JCCharnock/React-To-Do-List/static/js/2.e9aa7c3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JCCharnock/React-To-Do-List/static/js/2.e9aa7c3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "424725a07f1fe9d7d520",
+    "url": "/JCCharnock/React-To-Do-List/static/js/main.ec65866b.chunk.js"
+  },
+  {
+    "revision": "05b259e9397112a31a89",
+    "url": "/JCCharnock/React-To-Do-List/static/js/runtime-main.5a07fb82.js"
+  }
+]);
